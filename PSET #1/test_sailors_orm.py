df4e8f7e3ba9bfb6605fb485a7927a512761d9ec
Derek <51767748+DerekLeeCS@users.noreply.github.com>
2021-10-03 00:15:53 -0400

    Add unit test for expanded functionality

--- PSET #1/test_sailors_orm.py
+++ PSET #1/test_sailors_orm.py
@@ -253,3 +253,18 @@ def test_average_rating():
     results = session.execute(statement).fetchall()
 
     assert results == ans
+
+def test_get_one_star_reviews():
+    ans = [
+        ("I was charged the wrong amount and the owners refused to fix it.",),
+        ("Never coming back again.",),
+        ("My boat had a leak in it and I lost all of my stuff in the lake!",),
+    ]
+
+    statement = select(Review.contents) \
+        .select_from(Review) \
+        .where(Review.rating == 1) \
+        .order_by(Review.rsrvid)
+    results = session.execute(statement).fetchall()
+
+    assert results == ans
