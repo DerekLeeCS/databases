b4c97b1a1a1c14c7c868ad147497d2a15424508f
Derek <51767748+DerekLeeCS@users.noreply.github.com>
2021-10-30 13:59:34 -0400

    Refactor code to scrape information directly from product page

--- PSET #2/scraper.py
+++ PSET #2/scraper.py
@@ -1,58 +1,92 @@
+from typing import DefaultDict, Tuple
+from collections import defaultdict
 import requests
 import re
 import time
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/'
+url_to_scrape = 'http://books.toscrape.com/'
 
 
-def get_book_title(book):
-    description_content = book.find('h3').find('a', href=True)
+def get_website_data(url: str) -> BeautifulSoup:
+    """Sends an HTTP GET request to the specified URL and returns the response inside a BeautifulSoup object."""
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def get_book_title(book_soup: BeautifulSoup) -> str:
+    description_content = book_soup.find('h3').find('a', href=True)
     return description_content['title']
 
 
-def get_num_stars(book):
-    star_content = book.find('p')
+def get_num_stars(book_soup: BeautifulSoup) -> str:
+    # Need to do .next_sibling twice b/c it will grab white text
+    star_content = book_soup.find('p', {'class': 'instock availability'}).next_sibling.next_sibling
 
     # Finds the first match and extracts the string matching .*
     # E.g. <p class="star-rating Three"> -----> Three
     return re.search('<p class="star-rating (.*)">|$', str(star_content)).group(1)
 
 
-def get_price_and_availability(book):
-    price_and_availability_content = book.find('div', {'class': 'product_price'})
+def get_price_and_availability(book_soup: BeautifulSoup) -> Tuple[str, str]:
+    price_and_availability_content = book_soup.find('div', {'class': 'product_price'})
     price_content = price_and_availability_content.find('p', {'class': 'price_color'})
     price = price_content.get_text(strip=True)
-    # print(price)
     availability_content = price_and_availability_content.find('p', {'class': 'instock availability'})
     availability = availability_content.get_text(strip=True)
     is_available = availability == 'In stock'
-    # print(availability_content.get_text(strip=True) )
-    # print(price_content.encode_contents(formatter='html'))
     return price, is_available
+
+
+def get_book_data(file_url: str, book_info: BeautifulSoup) -> DefaultDict:
+    book_title = book_info['title']
+
+    # Go to book product page
+    book_url = file_url + '/../' + book_info['href']
+    book_soup = get_website_data(book_url)
+    book_soup = book_soup.find('article', {'class': 'product_page'})
+
+    # Need to do .next_sibling twice b/c it will grab white text
+    book_description = book_soup.find('div', {'id': 'product_description'}).next_sibling.next_sibling.get_text(strip=True)
+
+    # Store product information as a dictionary
+    book_information_dict = defaultdict(str)
+    book_information_rows = book_soup.find('table').find_all('tr')
+    for row in book_information_rows:
+        row_name = row.find('th').get_text(strip=True)
+        row_data = row.find('td').get_text(strip=True)
+        book_information_dict[row_name] = row_data
+
+    # Store other information
+    book_information_dict['Title'] = book_title
+    book_information_dict['Stars'] = get_num_stars(book_soup)
+    book_information_dict['Description'] = book_description
+
+    # Finds the first match and extracts the string matching .*
+    # E.g. In stock (19 available) -----> 19
+    book_information_dict['Availability'] = re.search('In stock \((.*) available\)|$', book_information_dict['Availability']).group(1)
+
+    return book_information_dict
+
 
 if __name__ == '__main__':
     # Scrape main website
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = get_website_data(url_to_scrape)
 
     # Get links to book categories
     book_categories = soup.find('div', {'class': 'side_categories'})
 
     # Iterate through each book category
     for link in book_categories.find_all('a', href=True):
-        file_url = url + link['href']
+        file_url = url_to_scrape + link['href']
         print("Scraping data from: ", file_url)
-
-        category_response = requests.get(file_url)
-        category_soup = BeautifulSoup(category_response.text, 'html.parser')
+        category_soup = get_website_data(file_url)
 
         # Iterate through each book
         for book in category_soup.find('body').find('ol').find_all('article', {'class': 'product_pod'}):
-            book_title = get_book_title(book)
-            num_stars = get_num_stars(book)
-            price, is_available = get_price_and_availability(book)
-            print(book_title, num_stars, price, is_available)
-        
+            book_info = book.find('h3').find('a', href=True)
+            book_info_dict = get_book_data(file_url, book_info)
+            print(book_info_dict)
+
         time.sleep(0.5)
         break
